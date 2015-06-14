69e1c547255aed8e2658b8533ade7d05fecdca08
João Neto <j.alexandreeneto@gmail.com>
2015-06-14 11:59:07 -0300

    Create ocorrencias.c

---
+++ ocorrencias.c
@@ -0,0 +1,14 @@
+int main(int argc, char **argv){
+}
+
+int validaoInteiro(char *str){
+}
+
+int registro(FILE *fp, int numero, char *local, char *tipo){
+}
+
+int consulta(FILE *fp, int numero){
+}
+
+float probabilidade(FILE *fp, char *local){
+}
