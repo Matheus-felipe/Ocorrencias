93437495972379cf174c45b37069fa517c727eff
João Neto <j.alexandreeneto@gmail.com>
2015-06-15 15:06:46 -0300

    Update ocorrencias.c

--- ocorrencias.c
+++ ocorrencias.c
@@ -1,7 +1,34 @@
+/*
+*	Projeto final de Lógica de Programação e Algoritmos I
+*		    	Controle de Ocorrências
+*	Alunos: João Neto, Matheus Ferreira e Anderson dos Santos
+*/
+
+#include <stdio.h>
+
+int validacaoInteiro(char *str);
+int registro(FILE *fp, int numero, char *local, char *tipo);
+int consulta(FILE *fp, int numero);
+int print_dados(FILE *fp, char *nome);
+
 int main(int argc, char **argv){
+
+	FILE *fp;
+	fp = fopen("dados", "wb");
+
 }
 
-int validaoInteiro(char *str){
+int validacaoInteiro(char *str){
+
+	int i = 0;
+
+	while(str[i]){
+		if(str[i] < '0' || str[i] > '9'){
+			return 0;
+		}
+		++i;
+	}
+	return 1;
 }
 
 int registro(FILE *fp, int numero, char *local, char *tipo){
@@ -10,5 +37,5 @@ int registro(FILE *fp, int numero, char *local, char *tipo){
 int consulta(FILE *fp, int numero){
 }
 
-float probabilidade(FILE *fp, char *local){
+float print_dados(FILE *fp, char *local){
 }
