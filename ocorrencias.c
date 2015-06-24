477f8c13f1b64da65ad3a6884a53350a63ff7182
João Neto <j.alexandreeneto@gmail.com>
2015-06-23 21:45:06 -0300

    Update ocorrencias.c

--- ocorrencias.c
+++ ocorrencias.c
@@ -9,7 +9,7 @@
 #include <string.h>
 
 struct Dados{
-	unsigned int num_controle;
+	int num_controle;
 	char local[40];
 	char tipo[20];
 };
@@ -18,56 +18,52 @@ struct Dados{
 int validacaoInteiro(char *str);
 void registro(FILE *fp, struct Dados *ocorrencias);
 void consulta(FILE *fp, int numero, struct Dados *ocorrencias);
-//int probabilidade(FILE *fp);
-int size_arq(FILE *fp, struct Dados teste);
-int city_ocorrencias(FILE *fp, char *cidade, struct Dados teste);
-
+int num_registros(FILE *fp);
+int city_ocorrencias(FILE *fp, char *cidade);
+float probabilidade(FILE *fp, char *cidade);
+void imprime_dados(FILE *fp, char *str);
+	
 int main(int argc, char **argv){
 
-	struct Dados info;
+	struct Dados info[1];
 	FILE *arq = NULL;
 
 	/* inicio do escopo de verificações */
 	if(argc != 5){
-		printf("Insira os parâmetros corretos!\n");
-		printf("<operação> <numero_controle> <local> <tipo>\n");
+		printf("Insira os parametros corretos!\n");
+		printf("<operacao> <numero_controle> <local> <tipo>\n");
 		return 1;		
 	}
-
 	if(!validacaoInteiro(argv[2]) || atoi(argv[2]) < 0){
-		printf("Entre com um número de controle válido.\n");
+		printf("Entre com um numero de controle valido.\n");
 		return 2;
 	}
 	/* fim do escopo de verificações */
 	
 	/* passando dados dos parâmetros de main para a estrutura */
-	info.num_controle = atoi(argv[2]);
-	strcpy(info.local,argv[3]);
-	strcpy(info.tipo, argv[4]);
+	info[0].num_controle = atoi(argv[2]);
+	strcpy(info[0].local,argv[3]);
+	strcpy(info[0].tipo, argv[4]);
 
+	/*condicoes para os tipos de operações */
 	if(!strcmp(argv[1],"reg")){
-		//arq = fopen("data.bin", "ab");
-		printf("Operação solicitada: %s\n", argv[1]);
-		registro(arq, &info);
+		registro(arq, &info[0]);
+		printf("Ocorrencia registrada!\n");
 	}
 	if(!strcmp(argv[1],"cons")){
-		printf("Operação solicitada: %s\n", argv[1]);
-		consulta(arq, info.num_controle, &info);
+		consulta(arq, info[0].num_controle, &info[0]);
+		printf("Número: %d\n", info[0].num_controle);
+		printf("Local: %s\n", info[0].local);
+		printf("Tipo: %s\n", info[0].tipo);
 	}
 	if(!strcmp(argv[1],"dados")){
-		printf("Operação solicitada: %s\n", argv[1]);
-		size_arq(arq,info);
+		imprime_dados(arq, argv[3]);
 	}
 	
-	//printf("%d", city_ocorrencias(arq, "Caruaru", info));
-
-	printf("Número: %d\n", info.num_controle);
-	printf("Local: %s\n", info.local);
-	printf("Tipo: %s\n", info.tipo);
-
 	return 0;
 }
 
+/* funcao que serve para validar o numero de controle */
 int validacaoInteiro(char *str){
 	
 	int i = 0;
@@ -84,46 +80,36 @@ void registro(FILE *fp, struct Dados *ocorrencias){
 
 	fp = fopen("data.bin", "ab");
 	
-	if(fp == NULL){
-		printf("O arquivo não pode ser aberto!\n");	
-	}
-	
 	fwrite(ocorrencias, 1, sizeof(struct Dados), fp);
 	fclose(fp);
+	
 }
 
 void consulta(FILE *fp, int numero, struct Dados *ocorrencias){
 	
 	fp = fopen("data.bin", "rb");
 	
-	if(fp == NULL){
-		printf("O arquivo não pode ser aberto!\n");		
-	}
-	
 	fseek(fp, numero*sizeof(struct Dados), SEEK_SET);
 	fread(ocorrencias, 1, sizeof(struct Dados), fp);
+	
 	fclose(fp);
 }
 
-int size_arq(FILE *fp, struct Dados a){
+int num_registros(FILE *fp){
 	
-	int pos, size;
+	int pos, tamanho;
 	fp = fopen("data.bin", "rb");
-	
-	if(fp == NULL){
-		printf("O arquivo não pode ser aberto!\n");
-	}
 	
 	fseek(fp, 0, SEEK_END);
 	pos = ftell(fp);
-	size = pos / sizeof(struct Dados);
+	tamanho = pos / sizeof(struct Dados);
 
 	fclose(fp);
-	
-	return size;
+
+	return tamanho;
 }
 
-int city_ocorrencias(FILE *fp, char *cidade, struct Dados teste){
+int city_ocorrencias(FILE *fp, char *cidade){
 	
 	int i;
 	int j = 4;
@@ -131,13 +117,9 @@ int city_ocorrencias(FILE *fp, char *cidade, struct Dados teste){
 	char city[40];
 	fp = fopen("data.bin", "rb");
 	
-	if(fp == NULL){
-		printf("O arquivo não pode ser aberto!\n");
-	}
-	
-	for(i = 0; i < size_arq(fp,teste); ++i){
+	for(i = 0; i < num_registros(fp); ++i){
 		
-		fseek(fp, 4, SEEK_CUR);
+		fseek(fp, j, SEEK_SET);
 		fread(&city, 1, 40, fp);
 		
 		if(!strcmp(city, cidade)){
@@ -152,8 +134,19 @@ int city_ocorrencias(FILE *fp, char *cidade, struct Dados teste){
 	
 }
 
-/* float probabilidade(FILE *fp, int qtd_cidade, int qtd_total, char *cidade, struct Dados teste){
+float probabilidade(FILE *fp, char *cidade){
 	
-	return city_ocorrencias(fp, cidade, teste);
+	return ((float)city_ocorrencias(fp, cidade) / num_registros(fp))*100;
 	
-} */
+} 
+
+void imprime_dados(FILE *fp, char *str){
+	
+	printf("Total de ocorrencias registradas: %d\n", num_registros(fp));
+	
+	if(strcmp(str, "none") != 0){
+		printf("%s teve %d ocorrencias. \n", str, city_ocorrencias(fp,str));
+		printf("Essa cidade possui %.2f %% de chance para que ocorra outra ocorrencia.\n", probabilidade(fp,str));
+	}
+	
+}
